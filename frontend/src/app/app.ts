9119137b16f32c87c565fa22a80de4d4547334f0
sergiics9 <sergi20031005@gmail.com>
2026-01-30 17:47:24 +0100

    Maquetado Login

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Login } from './components/auth/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('frontend');
--- frontend/src/app/components/auth/login/login.ts
+++ frontend/src/app/components/auth/login/login.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-
+  // Nueva forma de inyectar dependencias en Angular moderno
+  // private fb = inject(FormBuilder);
+  // // Creamos el formulario tipado y no-nulo
+  // loginForm = this.fb.nonNullable.group({
+  //   username: ['', Validators.required],
+  //   password: ['', Validators.required],
+  // });
+  // onSubmit() {
+  //   if (this.loginForm.valid) {
+  //     // .getRawValue() es seguro porque usamos nonNullable
+  //     const { username, password } = this.loginForm.getRawValue();
+  //     console.log('Login:', username, password);
+  //   } else {
+  //     this.loginForm.markAllAsTouched();
+  //   }
+  // }
 }
